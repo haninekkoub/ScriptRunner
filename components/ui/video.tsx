4a1c3a23861130ee39cca724eb74502d8a22a4b8
haninekkoub <haninekkoub@gmail.com>
2024-05-13 22:40:51 +0100

    fix : video ref

--- components/ui/video.tsx
+++ components/ui/video.tsx
@@ -5,19 +5,19 @@ import HlsPlayer from "react-hls-player";
 
 export default function Video({ playbackId }: { playbackId: string }) {
   if (!playbackId) return null;
+  const videoRef = useRef(null);
 
   const videoUrl = `https://stream.mux.com/${playbackId}.m3u8`;
-  const videoRef = useRef(null);
   return (
     <HlsPlayer
       className="h-[100vh] w-[100vw] absolute  inset-0 object-cover -z-20"
+      playerRef={videoRef}
       src={videoUrl}
       autoPlay
       muted
       playsInline
       loop
       controls={false}
-      playerRef={videoRef}
     />
   );
 }
